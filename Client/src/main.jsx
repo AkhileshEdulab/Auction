5ee7bf8c227ea7c104a7edb0ccf7b49f836e935b
Akhilesh Yadav <akhilesh@edulab.in>
2025-11-13 15:51:01 +0530

    Fix case-sensitive import for Store.js

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './Stores/store.js';
+import { store } from './Stores/Store.js';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
